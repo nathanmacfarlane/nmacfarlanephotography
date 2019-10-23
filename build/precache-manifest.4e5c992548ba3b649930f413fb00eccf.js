2403290a74a5190573672859d00eea6110326249
Nathan Macfarlane <nathanmmacfarlane@gmail.com>
2019-10-23 16:28:59 -0700

    definitely build folder

---
+++ build/precache-manifest.4e5c992548ba3b649930f413fb00eccf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb7e24af2c3165c0f8b56afea54d4b95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "480ed5bcaeda5fb9e9dd",
+    "url": "/static/css/main.34f25b6b.chunk.css"
+  },
+  {
+    "revision": "2e66d1d781337c6597c8",
+    "url": "/static/js/2.5ef6eac4.chunk.js"
+  },
+  {
+    "revision": "480ed5bcaeda5fb9e9dd",
+    "url": "/static/js/main.6a93b874.chunk.js"
+  },
+  {
+    "revision": "5aa45b48a71cb1f56634",
+    "url": "/static/js/runtime-main.ffb0bc24.js"
+  },
+  {
+    "revision": "fd725012cbd147b73144a76607231688",
+    "url": "/static/media/me.fd725012.jpg"
+  }
+]);
